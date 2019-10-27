3f7d31bfc18cf817e2463305723a30fdc6e292f5
Eduardo Henrique Vieira dos Santos <eduuu_santos@hotmail.com>
2019-10-27 16:03:52 -0300

    Save and load Network, Training and Test error information and parameters, multi-line prediction. Todo: refactor

--- trainingArea/js/mysyn.js
+++ trainingArea/js/mysyn.js
@@ -57,35 +57,87 @@ function newNetwork(lengths, forwardfull_connected = false){
 }
 
 // train the network
-function learnBPP(net, inputs, outputs, loops = 20000, learningRate = 0.3){
+async function learnBPP(net, inputs, outputs, loops = 20000, learningRate = 0.3){
 	var history = [];
 
 	var trainer = new Trainer(net);
 	var trainingSet = [];
-	for(var i = 0; i < inputs.length; i++){
+	var testSet = [];
+	var validationSet = {'input':[],'output':[]};
+	var trainingSetVal = {'input':[],'output':[]};
+	var training_len = 0;
+	var train_data_proportion = parseFloat(document.querySelector('#train_data_proportion').value);
+	var validate_error = false;
+	var validate_each = parseInt(document.querySelector('#test_count').value);
+	var log_interval = parseInt(document.querySelector('#log_interval').value);
+	var cost_function = document.querySelector('#const_function [selected]').value;
+	if (inputs.length>100){
+		training_len = parseInt(inputs.length*train_data_proportion);
+	} else {
+		training_len = inputs.length;
+	}
+	for(var i = 0; i < training_len; i++){
 		trainingSet[trainingSet.length] = {'input':inputs[i],'output':outputs[i]};
+		testSet[testSet.length] = {'input':inputs[i],'output':outputs[i]};
+	}
+	if (training_len != inputs.length) {
+		validate_error = true;
+		for(var i = training_len; i < inputs.length; i++){
+			validationSet['input'][validationSet['input'].length] = inputs[i];
+			validationSet['output'][validationSet['output'].length] = outputs[i];
+		}
+		for(var i = 0; i < training_len; i++){
+			trainingSetVal['input'][trainingSetVal['input'].length] = inputs[i];
+			trainingSetVal['output'][trainingSetVal['output'].length] = outputs[i];
+		}
+	} else {
+		document.querySelector('.vcost').innerHTML = "Samples < 100.."
+		document.querySelector('.tcost').innerHTML = "Samples < 100.."
 	}
 	// Async training
-	trainer.trainAsync(trainingSet,{
-		rate: learningRate,
-		iterations: loops,
-		error: .0001,
-		log: 1000,
-		cost: Trainer.cost.CROSS_ENTROPY,
-		schedule: {
-			every: 500, // repeat this task every 500 iterations
-			do: function(data) {
-				// custom log
-				var logstr = "Error: "+data.error+"| Iterations:"+data.iterations+"| Rate:"+data.rate;
-				console.log("error", data.error, "iterations", data.iterations, "rate", data.rate);
-				document.querySelector('.error').innerHTML = data.error;
-				document.querySelector('.iterations').innerHTML = data.iterations;
-				document.querySelector('.rate').innerHTML = data.rate;
-				if (data.error<0.00001)
-					return true; // abort/stop training
+	document.querySelector('.iterations').innerHTML = 0;
+	for (var i = 0; i < parseInt(validate_each); i++) {
+		await trainer.trainAsync(trainingSet,{
+			rate: learningRate,
+			iterations: parseInt(loops/validate_each),
+			error: .0001,
+			log: 1000,
+			cost: Trainer.cost[cost_function],
+			schedule: {
+				every: log_interval, // repeat this task every 500 iterations
+				do: function(data) {
+					// custom log
+					var logstr = "Error: "+data.error+"| Iterations:"+data.iterations+"| Rate:"+data.rate;
+					console.log("error", data.error, "iterations", data.iterations, "rate", data.rate);
+					document.querySelector('.terror').innerHTML = data.error;
+					document.querySelector('.iterations').innerHTML = parseInt(document.querySelector('.iterations').innerHTML)+log_interval;//data.iterations;
+					if (data.error<0.00001)
+						return true; // abort/stop training
+					}
 				}
+		}).then(results => {
+			//var test_error = trainer.test(testSet).error;
+			document.querySelector('.test_error').innerHTML = trainer.test(testSet).error;
+			var validation_error = 0;
+			if (validate_error) {
+				var validation_activation = []; 
+				for (var i = 0; i < trainingSetVal['input'].length; i++) {
+					validation_activation[validation_activation.length] = net.activate(trainingSetVal['input'][i]);
+				}
+				validation_error = Trainer.cost[cost_function](trainingSetVal['output'], validation_activation);
+				document.querySelector('.tcost').innerHTML = validation_error;
+
+				validation_activation = []; 
+				for (var i = 0; i < validationSet['input'].length; i++) {
+					validation_activation[validation_activation.length] = net.activate(validationSet['input'][i]);
+				}
+				validation_error = Trainer.cost[cost_function](validationSet['output'], validation_activation);
+				document.querySelector('.vcost').innerHTML = validation_error;
 			}
-	}).then(results => {console.log('done!', results);return history});
+			console.log('done!', results);return history
+		});
+
+	}
 	/*
 	// Backpropagation__
 	// iterates over the loops counting
--- trainingArea/js/utils.js
+++ trainingArea/js/utils.js
@@ -1,13 +1,35 @@
 
-var net;
 var learning_history;
+var trainInputMinMax;
+var trainOutputMinMax;
 var myNetwork = null;
 
 function reset() {
-	document.querySelector('.error').innerHTML = '0.0';
+	document.querySelector('.terror').innerHTML = '0.0';
 	document.querySelector('.iterations').innerHTML = '0.0';
-	document.querySelector('.rate').innerHTML = '0.0';
+	document.querySelector('.verror').innerHTML = '0.0';
 	myNetwork = null;
+}
+
+function getMatrix(elem)	{
+	var res = document.getElementById(elem).value.trim();
+	res = res.replace(/\n/g, "],[");
+	res = res.replace(/\t/g, ",");
+	res = "[["+res+"]];";	
+	return res;
+}
+
+function setDownloadString(str, elem)	{
+	var linkstr = "data:application/octet-stream,";
+	linkstr += encodeURI(str);
+	document.querySelector(elem).href = linkstr;
+}
+
+function saveNetwork() {
+	if (myNetwork) {
+		var netStr = JSON.stringify(myNetwork);
+		setDownloadString(netStr, '#saveNetwork');
+	}
 }
 
 function learn(){
@@ -16,10 +38,10 @@ console.log('learn()');
 	netlen = "netlen = ["+document.getElementById('netlen').value+"];";
 	loops = "loops = "+document.getElementById('loops').value+";";
 	rate = "rate = "+document.getElementById('rate').value+";";
-	var res = document.getElementById('inpmatx').value.trim().replace(/\n/g, "],[");
-	inpmatx = "inpmatx = [["+res+"]];";
-	var res = document.getElementById('outmatx').value.trim().replace(/\n/g, "],[");
-	outmatx = "outmatx = [["+res+"]];";
+	var res = getMatrix('inpnotnorm');
+	inpmatx = "inpmatx = "+res+";";
+	var res = getMatrix('outnotnorm');
+	outmatx = "outmatx = "+res+";";
 	appendBody('script', netlen);
 	appendBody('script', loops);
 	appendBody('script', rate);
@@ -37,54 +59,101 @@ console.log('learn()');
 		outputs = outmatx,
 		loops = loops,
 		learningRate = rate);
+	document.querySelector('#saveButton').style.display = 'block';
 	return myNetwork;
 }
 
 function tryit(n){
 	console.log('tryit('+n+')');
-	var tryit_act =  "try_val = ["+document.getElementById('tryit').value+"];";
-	appendBody('script', tryit_act);
-
-	var res = n.activate(try_val);
-	console.log(try_val);
-	console.log(res);
-	document.getElementById('res').value = res;
+	//var tryit_act =  "try_val = ["+document.getElementById('tryit').value+"];";
+	//appendBody('script', tryit_act);
+	var aux = document.getElementById('tryit').value.trim().split('\n');
+	var try_val = [];
+	for (var i = 0; i < aux.length; i++) {
+		var columns = aux[i].split('\t')
+		for (var j = 0; j < columns.length; j++) {
+			columns[j] = parseFloat(columns[j]);
+		}
+		try_val[try_val.length] = columns;
+	}
+	document.getElementById('res').innerHTML = '';
+	for (var i = 0; i < try_val.length; i++) {
+		if(trainInputMinMax){
+			try_val[i] = scaleVector(try_val[i], trainInputMinMax);	
+		}
+		var res = n.activate(try_val[i]);
+		if(trainOutputMinMax){
+			res = unscale(res,trainOutputMinMax);
+		}
+		document.getElementById('res').innerHTML += res.toString().replace(/,/g, '\t')+'\n';
+	}
 }
 
-	function normalizedata(elem, target) {
-	console.log('normalizedata('+elem+','+target+')');
-	elem = document.getElementById(elem).value;
-	var res = elem.replace(/\n/g, "],[");
-	var dtx = "datamatrix = [["+res+"]];";
-	appendBody('script', dtx);
+function fitMinMaxScaler(dtMatrix)	{
 	var maxincolumn = [];
-	for (var i = 0; i < datamatrix[0].length; i++){
-		maxincolumn.push(datamatrix[0][i]);
+	for (var i = 0; i < dtMatrix[0].length; i++){
+		maxincolumn.push(dtMatrix[0][i]);
 	}
 	var minincolumn = [];
-	for (var i = 0; i < datamatrix[0].length; i++){
-		minincolumn.push(datamatrix[0][i]);
+	for (var i = 0; i < dtMatrix[0].length; i++){
+		minincolumn.push(dtMatrix[0][i]);
 	}
 
-	for (var i = 0; i < datamatrix.length; i++) {
-		for (var j = 0; j < datamatrix[0].length; j++) {
-			if(datamatrix[i][j] > maxincolumn[j]) {
+	for (var i = 0; i < dtMatrix.length; i++) {
+		for (var j = 0; j < dtMatrix[0].length; j++) {
+			if(dtMatrix[i][j] > maxincolumn[j]) {
 				// Max value from each column
-				maxincolumn[j] = datamatrix[i][j];
+				maxincolumn[j] = dtMatrix[i][j];
 			}
-			if(datamatrix[i][j] < minincolumn[j]) {
+			if(dtMatrix[i][j] < minincolumn[j]) {
 				// Min value from each column
-				minincolumn[j] = datamatrix[i][j];
+				minincolumn[j] = dtMatrix[i][j];
 			}
 		}
 	}
-	// Normalize the data
-	for (var i = 0; i < datamatrix.length; i++) {
-		for (var j = 0; j < datamatrix[i].length; j++) {
-			xi = datamatrix[i][j];
-			datamatrix[i][j] = (xi - minincolumn[j])/(maxincolumn[j]-minincolumn[j]);
+	return {maxincolumn, minincolumn};
+}
+
+function scale(dtMatrix, minMaxColumns)	{
+	for (var i = 0; i < dtMatrix.length; i++) {
+		dtMatrix[i] = scaleVector(dtMatrix[i], minMaxColumns);
+	}
+	return dtMatrix;
+}
+function scaleVector(vector, minMaxColumns)	{
+	for (var j = 0; j < vector.length; j++) {
+		xi = vector[j];
+		var amplitude = (minMaxColumns.maxincolumn[j]-minMaxColumns.minincolumn[j]);
+		var observation = (xi - minMaxColumns.minincolumn[j]);
+		if (observation < 0) {
+			observation = 0;
+		}
+		vector[j] = observation/amplitude;
+		if (vector[j] > 1) {
+			vector[j] = 1;
 		}
 	}
+	return vector;
+}
+function unscale(vector, minMaxColumns)	{
+	for (var j = 0; j < vector.length; j++) {
+		var amplitude = (minMaxColumns.maxincolumn[j]-minMaxColumns.minincolumn[j]);
+		vector[j] = vector[j] * amplitude;
+		vector[j] = vector[j] + minMaxColumns.minincolumn[j];
+	}
+	return vector;
+}
+
+function scaleData(elem, target) {
+	console.log('normalizedata('+elem+','+target+')');
+
+	var res = getMatrix(elem);
+	var dtx = "datamatrix = "+res+";";
+
+	appendBody('script', dtx);
+	var minMaxScaler = fitMinMaxScaler(datamatrix);
+	// Normalize the data
+	datamatrix = scale(datamatrix, minMaxScaler)
 	// Write into target textarea
 	document.getElementById(target).value = "";
 	for (var i = 0; i < datamatrix.length; i++) {
@@ -94,12 +163,13 @@ function tryit(n){
 		for (var j = 0; j < datamatrix[i].length; j++) {
 			document.getElementById(target).value += datamatrix[i][j];
 			if (j < datamatrix[i].length-1){
-				document.getElementById(target).value += ",";
+				document.getElementById(target).value += "\t";
 			}
 			//xi = datamatrix[i][j];
 			//datamatrix[i][j] = (xi - minincolumn[j])/(maxincolumn[j]-minincolumn[j]);
 		}
 	}
+	return minMaxScaler;
 }
 
 function appendBody(elem, content) {
@@ -109,3 +179,32 @@ function appendBody(elem, content) {
 	node.appendChild(textnode);
 	document.body.appendChild(node);
 }
+
+function loadNetworkFromJsonFile(evt) {
+	var files = evt.target.files; // FileList object
+	// Loop through the FileList and get the json netowrk
+	for (var i = 0, f; f = files[i]; i++) {
+		// Only process image files.
+		if (!f.type.match('application/json')) {
+			alert('Invalid file type. Choose a JSON file.')
+		}
+
+		var reader = new FileReader();
+
+		// Closure to capture the file information.
+		reader.onload = (function(theFile) {
+			return function(e) {
+				// Render thumbnail.
+				var filestream = e.target.result.toString();
+				var netAux = filestream.split(',')[1];
+				netAux = atob(netAux); // from Base64 to text
+				netAux = JSON.parse(netAux);
+				myNetwork = Network.fromJSON(netAux);
+			};
+		})(f);
+		// Read in the image file as a data URL.
+		reader.readAsDataURL(f);
+	}
+}
+
+document.getElementById('files').addEventListener('change', loadNetworkFromJsonFile, false);
